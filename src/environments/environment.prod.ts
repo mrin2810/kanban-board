650d7872238ed5836ce66a60d33bc8e5d30cb5f1
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-09-23 19:08:00 +0530

    added firebase to the project and connected with the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKNmEcz-43Io7dTBfqz3bSMU4Kx8cl8xI",
+    authDomain: "kanban-board-9ef51.firebaseapp.com",
+    projectId: "kanban-board-9ef51",
+    storageBucket: "kanban-board-9ef51.appspot.com",
+    messagingSenderId: "827321023203",
+    appId: "1:827321023203:web:184a8359844ef00d9fe3d6",
+    measurementId: "G-3YTBJ4DG0B"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAKNmEcz-43Io7dTBfqz3bSMU4Kx8cl8xI",
+    authDomain: "kanban-board-9ef51.firebaseapp.com",
+    projectId: "kanban-board-9ef51",
+    storageBucket: "kanban-board-9ef51.appspot.com",
+    messagingSenderId: "827321023203",
+    appId: "1:827321023203:web:184a8359844ef00d9fe3d6",
+    measurementId: "G-3YTBJ4DG0B"
+  }
 };
 
 /*
