e109fa21f32fe56a3b7e690065784c39806ed2d1
unknown <Administrator@ws-02-05-004.zeuslearning.mumbai>
2021-09-24 19:13:35 +0530

    added functionality for signup

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -12,7 +12,7 @@ export class EmailLoginComponent implements OnInit {
   type: 'login' | 'signup' | 'reset' = 'signup';
   loading = false;
 
-  serverMessage!: string;
+  serverMessage: any;
 
   constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) { }
 
@@ -64,7 +64,29 @@ export class EmailLoginComponent implements OnInit {
   }
 
   async onSubmit() {
-    
+      this.loading = true;
+
+      const email = this.email?.value;
+      const password = this.password?.value;
+   
+      try {
+        if (this.isLogin) {
+          await this.afAuth.signInWithEmailAndPassword(email, password);
+        }
+
+        if (this.isSignup) {
+          await this.afAuth.createUserWithEmailAndPassword(email, password);
+        }
+
+        if (this.isPasswordReset) {
+          await this.afAuth.sendPasswordResetEmail(email);
+          this.serverMessage = 'Check your email';
+        }
+      } catch (err) {
+        this.serverMessage = err;
+      }
+
+      this.loading = false;
   }
 
 }
