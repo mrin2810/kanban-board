ffe826bcd5f1090a77b4c3d4f07093831d1ea045
mrin2810 <mrinmayeerane2810@gmail.com>
2021-09-25 00:45:05 +0530

    added kanban module as lazy-loading module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
   {
     path: 'login', 
     loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'kanban',
+    loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule)
   }
 ];
 
