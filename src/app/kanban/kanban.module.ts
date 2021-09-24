d055af9fa30d0785c1ecb6fac56ebf9c5321c49b
mrin2810 <mrinmayeerane2810@gmail.com>
2021-09-25 00:48:58 +0530

    added shared module and other imports to kanban

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { KanbanRoutingModule } from './kanban-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    KanbanRoutingModule
+    KanbanRoutingModule,
+    SharedModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggleModule
   ]
 })
 export class KanbanModule { }
