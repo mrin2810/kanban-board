867693237ad94db8833a765b91d55a93bf7325e4
mrin2810 <mrinmayeerane2810@gmail.com>
2021-09-25 01:18:07 +0530

    added board service and add req modules to it

---
+++ src/app/kanban/board.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import * as firebase from 'firebase/compat/app';
+import { switchMap, map } from 'rxjs/operators';
+import { Board, Task } from './board.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
+
+  // create a new board
+  async createBoard(data: Board) {
+    const user = await this.afAuth.currentUser;
+    return this.db.collection('boards').add({
+      ...data,
+      uid: user?.uid,
+      tasks: [{ description: 'Hello!', label: 'yellow' }]
+    });
+  }
+  //delete a board
+  deleteBoard(boardId: string) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .delete();
+  }
+}
