9d5eaf4a413250d563828e0be48b90bb714de7fc
unknown <Administrator@ws-02-05-004.zeuslearning.mumbai>
2021-09-24 16:53:29 +0530

    added google-signin logic with firebase

--- src/app/user/google-signin.directive.ts
+++ src/app/user/google-signin.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/compat/app';
 
 @Directive({
   selector: '[appGoogleSignin]'
 })
 export class GoogleSigninDirective {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
+  @HostListener('click')
+  onclick() {
+    this.afAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider());
+  }
 }
